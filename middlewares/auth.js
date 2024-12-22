f5f3c3c066f9e7dab15da3dba29bcc9fae06ee82
abhishek yadav <abhisathayadav143@gmail.com>
2024-12-22 20:57:52 +0530

    ok

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.header("Authorization")?.split(" ")[1];
+  // console.log(req.header("authorization"))
+  const token = req.header("authorization")?.split(" ")[1];
   if (!token) 
     {
       return res.status(401).json({ message: "Access denied, token missing" });
